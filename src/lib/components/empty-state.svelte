4e352ccc88a95903ceb6182579464863f2aea486
Gérald Lbn <gerald.leban0@gmail.com>
2025-08-20 20:19:09 +0200

    feat: add shadcn svelte

---
+++ src/lib/components/empty-state.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+
+  const { icon, title, description, action }: {
+    icon?: Snippet<[]>
+    title: string;
+    description?: string
+    action?: Snippet<[]>
+  } = $props()
+</script>
+
+
+<div class="bg-secondary flex flex-col gap-4 items-center justify-center p-4 rounded-lg h-96">
+  {@render icon?.()}
+  <div class="text-center">
+    <h2 class="text-lg font-semibold text-foreground">{title}</h2>
+    {#if description}
+      <p class="text-sm text-muted-foreground">{description}</p>
+    {/if}
+  </div>
+  {@render action?.()}
+</div>
